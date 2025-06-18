729474ae786231c507e8739ae3da9ca0ea8881b3
surya-teja <suryachandrateja444@gmail.com>
2025-06-18 13:40:53 +0530

    updated layout code(removed commented code)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,3 @@
-// import { FontAwesome } from '@expo/vector-icons';
-// import { Tabs } from 'expo-router';
-
-// export default function Layout() {
-//   return (
-//     <Tabs>
-//       <Tabs.Screen
-//         name="audio"
-//         options={{
-//           title: 'Audio Library', 
-//           tabBarIcon: ({ color }) => <FontAwesome name="music" size={24} />,
-//          }} />
-//       <Tabs.Screen
-//         name="files"
-//         options={{
-//           title: 'Documents',
-//           tabBarIcon: ({ color }) => <FontAwesome name="file" size={24} color="black" />,
-//          }} />
-//     </Tabs>
-//   );
-// }
 import { FontAwesome } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
